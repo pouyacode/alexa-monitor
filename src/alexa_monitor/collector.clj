b24a85640ac288d5188a5ff078ba2be90eba3051
Pouya Abbassi <abbassi.pouya@gmail.com>
2021-06-25 00:51:21 +0430

    More comments

--- src/alexa_monitor/collector.clj
+++ src/alexa_monitor/collector.clj
@@ -1,3 +1,9 @@
+;;; Here we handle everything relative to crawling the webpage and extracting the parts we need.
+;;; Let's choose cool function names; If it's about getting some html page, why not call it `web-grab`?!
+;;;
+;;; Here we need 3 different things from the webpage, so we have one function for each!
+;;; They look a lot alike, but I decided not to mix them into one function, so the result would be easier to read and maintain.
+;;; Maybe later we can find a way to remove duplication without making it look messy.
 (ns alexa-monitor.collector
   (:require [hato.client :as web]
             [hickory.core :as hick]
@@ -5,11 +11,11 @@
   (:gen-class))
 
 
+;;; It returns `java.lang.Long`.
+;;; I used an `or` to return `0` if there wasn't any digit in the string.
+;;; Because java's `Long` can be `nil` sometimes!
 (defn digitize
-  "Removes any non-digit from string.
-  Returns `java.lang.Long`.
-  I used `or` to return `0` if there wasn't any digit in the string.
-  Because java's `Long` can be `nil` sometimes."
+  "Trim string and return its digit part."
   [string]
   (or 
    (->> string
@@ -19,13 +25,17 @@
    0))
 
 
+;;; Creates a simple HTTP GET request, ignores the status and only returns the HTML part.
+;;;
+;;; TODO: Error handling.
 (defn web-grab
-  "Retrieve the page and returns html output. `hiccup` of its `body` element."
+  "Retrieve the page and returns html output."
   [url]
   (-> (web/get url)
       :body))
 
 
+;;; Let's have 'hickory' handle everything.
 (defn hiccupize
   "Generate hiccup from html input."
   [html]
@@ -34,6 +44,7 @@
       hick/as-hickory))
 
 
+;;; Extract the rank of our domain from the webpage and `gigitize` it.
 (defn rank
   "Proces the hiccup and read `Alexa Rank`"
   [hiccup]
@@ -47,6 +58,7 @@
       digitize))
 
 
+;;; Extract the backlink count, then `digitize` it.
 (defn backlink
   "Process the hiccup and read `Sites Linking In`."
   [hiccup]
@@ -61,6 +73,8 @@
       digitize))
 
 
+;;; I think it's easier to read sitename from webpage, than use some faulty regex to remove `http` and `www` and `/something` part
+;;; out of user's input.
 (defn sitename
   "Get sitename from webpage, without `http` and other parts."
   [hiccup]
@@ -71,12 +85,17 @@
       first))
 
 
-(defn hash-mapize [[site rank bl]]
+;;; Later we'll insert this hash-map directly into database.
+;;; It's exactly how `database/new-entry` wants it!
+(defn hash-mapize
+  "Take everything and put them into a single hash-map."
+  [[site rank bl]]
   {:site site
    :rank rank
    :backlink bl})
 
 
+;;; Creates a nice data-flow through every function of this namespace.
 (defn main
   "Entry point."
   [url]
