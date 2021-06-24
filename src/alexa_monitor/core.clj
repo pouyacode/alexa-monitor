bb790433022a267fd2a8a7912c36a693fe6ab6a0
Pouya Abbassi <abbassi.pouya@gmail.com>
2021-06-24 09:56:10 +0430

    Better comments

--- src/alexa_monitor/core.clj
+++ src/alexa_monitor/core.clj
@@ -1,17 +1,26 @@
+;;; Obviously, the entry point of our application.
+;;; Here we import `collector` and `database` namespace and call some functions within them.
 (ns alexa-monitor.core
   (:require [alexa-monitor.collector :as collector]
             [alexa-monitor.database :as database])
   (:gen-class))
 
 
+;;; You can run a simple webserver using `python3 -m http.server 8888` inside `resources` directory.
+;;; So that our tests and development won't create actual requests to Alexa's servers.
+;;; For creating `uberjar` or testing the actual result, change the `http://localhost:8080` to `https://www.alexa.com/minisiteinfo/domain.com`.
+;;; For now, it works with a single hard-coded url.
+;;;
+;;; This functions sends our url to `collector/main` and gets our last database record from `database/last-rank`.
+;;; If our last record and the crawled data were not equal, it creates a new entry in database.
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Crawl the page and grab some useful data via `alexa-monitor.collector` and put those data in database, using `alexa-monitor.database`."
   [& args]
-  (let [info (-> "http://localhost:8080"
+  (let [info (-> "http://localhost:8080" ;Change this to `https://www.alexa.com/minisiteinfo/domain.com` to create actual requests.
                  collector/main)
-        last-db (database/last-rank (:site info))
-        changed? (not= last-db (:rank info))]
+        last-db (database/last-rank (:site info)) ;Use the "site name" inside the crawled page, to get latest data from local database.
+        changed? (not= last-db (:rank info))] ;Check if newly crawled data is the same as local database's latest record.
     (if changed?
-      (database/new-entry database/db
+      (database/new-entry database/db   ;Submit new rank to local database.
                           :rank
                           info))))
--- src/alexa_monitor/database.clj
+++ src/alexa_monitor/database.clj
@@ -1,14 +1,23 @@
+;;; Small namespace with few useful functions to work with database.
 (ns alexa-monitor.database
   (:require [clojure.java.jdbc :refer :all])
   (:gen-class))
 
 
+;;; We currently use sqlite. Since it's a small project, even a CSV file could do the trick.
+;;; But let's keep things professional!
+;;; MySQL or similar database would eliminate some troubles we have with `java.io.File` and its weird behavior
+;;; on different stages of our project (repl, uberjar, native-image), but we'll try to make it work.
+;;; If we couldn't go on, we'll switch to MySQL (or preferably PostgrSQL)
 (def db
   {:classname "org.sqlite.JDBC"
    :subprotocol "sqlite"
    :subname (.getAbsolutePath (new java.io.File "db/database.db"))})
 
 
+;;; For now, we create the `db` directory and run this function manually (in repl) when we need to create database.
+;;; Check out [this (beginner friendly) issue](https://github.com/pouyacode/alexa-monitor/issues/2) if you want to write error handling
+;;; for this part and create directory and database automatically if the software couldn't find database.
 (defn create-db
   "Create database and table."
   []
@@ -23,15 +32,8 @@
          (println (.getMessage e)))))
 
 
-(defn print-result-set
-  "Print the result set in tabular form."
-  [result-set]
-  (doseq [row result-set]
-    (println row)))
-
-
 (defn last-rank
-  "Execute query and return lazy sequence."
+  "Get the last recorded rank for given domain name. It returns a lazy sequence."
   [site]
   (-> db
       (query [(str "SELECT rank FROM rank where site='" site "' order by id desc limit 1")])
