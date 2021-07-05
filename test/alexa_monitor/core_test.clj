e0f9459d300e3bde85a163227ec546ee4e7fb642
Pouya Abbassi <abbassi.pouya@gmail.com>
2021-07-05 21:01:19 +0430

    Move tests to relevent file

--- test/alexa_monitor/core_test.clj
+++ test/alexa_monitor/core_test.clj
@@ -1,78 +1,8 @@
 (ns alexa-monitor.core-test
   (:require [clojure.test :refer :all]
-            [alexa-monitor.core :refer :all]
-            [alexa-monitor.collector :refer :all]))
-
-
-(def html
-  "<!DOCTYPE html>
-<html lang=\"en\">
-<head><title>Alexa - Browser Extension</title></head>
-<body data-cache-prefix=\"http://localhost/alx-sa-c0d6d28c-1620928494\" id=\"Extension\" class=\"Extension  noWidget\">
-  <header class=\"flex\">
-    <div class=\"FourthFull\">
-        <i class=\"fa fa-globe\" aria-hidden=\"true\"></i>
-        <h2>
-            <a class=\"white nounderline truncation\" href=\"/siteinfo/pouyacode.net\" target=\"_blank\" >pouyacode.net</a>
-        </h2>
-    </div>
-    <a class=\"white nounderline\" href=\"/siteinfo/pouyacode.net\" target=\"_blank\" ><img src=\"/images/logos/logo-white.png\" style=\"height:20px;\"/></a>
-  </header>
-  <section class=\"flex nopaddingbottom\">
-    <div style=\"width:65%;\">  
-      <p class=\"textbig\">Alexa Rank</p>
-        <a href=\"/siteinfo/pouyacode.net\" target=\"_blank\" class=\"big data\"><span class=\"hash\">#</span>9,397,083 </a>
-        <hr>
-        <p class=\"textbig nomargin\">Sites Linking In: <a href=\"/siteinfo/pouyacode.net\" target=\"_blank\" class=\"small data\">1</a></p>
-    </div> 
-    <div style=\"width:35%;\">  
-      <p class=\"textbig\">Similar Sites</p>
-      <p>No data</p>
-    </div>
-  </section>
-</body>
-</html>")
+            [alexa-monitor.core :refer :all]))
 
 
 (deftest test-universe
   (testing "Let's see if the universe is as sane as we want."
     (is (= true (not false)))))
-
-
-(deftest test-hiccupize
-  (testing "Just simple test, I don't think we need more."
-    (is (= :document (-> html
-                         hiccupize
-                         :type)))))
-
-
-(deftest test-parse
-  (testing "Rank test."
-    (is (= 9397083 (-> html
-                            hiccupize
-                            rank))))
-  (testing "Backlink test."
-      (is (= 1 (-> html
-                   hiccupize
-                   backlink))))
-  (testing "Sitename test."
-      (is (= "pouyacode.net" (-> html
-                                 hiccupize
-                                 sitename)))))
-
-
-(deftest test-digitize
-  (testing "123"
-    (is (= 123 (digitize "123"))))
-  (testing "123,456"
-    (is (= 123456 (digitize "123,456"))))
-  (testing "Empty"
-    (is (= 0 (digitize "Anything"))))
-  (testing "Not trimmed"
-    (is (= 42 (digitize "\n\t 42 \r\n\b")))))
-
-
-#_(test-huccupize)
-#_(test-finder)
-#_(test-digitize)
-#_(test-backlink)
